5b19c70ef11e4fbf2460c92760d6f7323c22f278
Andrew Sugito <andrew.sugito2@gmail.com>
2025-10-28 21:44:48 +1100

    Add folder structure and installed tailwindcss + react router

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+
+function LandingPage() {
+    return (
+        <>
+            <div className="flex justify-center items-center">
+                <h1 className="text-3xl font-bold underline">Landing Page</h1>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage;
